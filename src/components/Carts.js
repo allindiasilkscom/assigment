b788f443f7e5d18e0e5481578fb4141faeb0c9d0
allindiasilkscom <153381149+allindiasilkscom@users.noreply.github.com>
2024-01-30 12:27:00 +0530

    Update Carts.js

--- src/components/Carts.js
+++ src/components/Carts.js
@@ -1,4 +1,6 @@
+// Carts.js
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import cardsData from "./CardsData";
@@ -18,20 +20,33 @@ const Carts = () => {
       <div className="row d-flex justify-content-center align-items-center">
         {data.map((element, id) => {
           return (
-            <Card key={id} style={{ width: "22rem", border: "none" }} className="mx-2 mt-4 card_style">
-              <Card.Img variant="top" src={element.imgdata} style={{ height: "16rem" }} className="mt-3" />
+            <Card
+              key={id}
+              style={{ width: "22rem", border: "none" }}
+              className="mx-2 mt-4 card_style"
+            >
+              <Card.Img
+                variant="top"
+                src={element.imgdata}
+                style={{ height: "16rem" }}
+                className="mt-3"
+              />
               <Card.Body>
                 <Card.Title>{element.rname}</Card.Title>
-                <Card.Text>
-                  Price: ₹{element.price}
-                </Card.Text>
+                <Card.Text>Price: ₹{element.price}</Card.Text>
                 <div className="button_div d-flex justify-content-center">
                   <Button
                     variant="primary"
                     className="col-lg-12"
                     onClick={() => addToCart(element)}
                   >
-                    Add To Cart
+                    {/* Use Link inside the Button's onClick event */}
+                    <Link
+                      to="/cart"
+                      style={{ color: "white", textDecoration: "none" }}
+                    >
+                      Add To Cart
+                    </Link>
                   </Button>
                 </div>
               </Card.Body>
